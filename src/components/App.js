c79fa4d2d7605c85c44ea587ca665cb39e71db2d
sabahat-123 <sabahat.parveen@imtspl.net>
2022-12-08 20:08:29 +0530

    done

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,41 @@
 import React, {useEffect, useState} from 'react'
 import '../styles/App.css';
 
-const App = () => {
-//code here 
-  return (
-    <div id="main">
-      <p className={} >Newton School</p>
-      <button id='button' onClick={}>Change Style</button>
-    </div>
-  )
+// const App = () => {
+// //code here 
+
+//   return (
+//     <div id="main">
+//       <p className={onchange} >Newton School</p>
+//       <button id='button' onClick={}>Change Style</button>
+//     </div>
+//   )
+// }
+const red = 'red';
+const blue = 'blue';
+
+export default class App extends React.Component{
+  constructor(props){
+    super(props);
+    this.state = { color: red };
+    this.changeColor = this.changeColor.bind(this);
+  }
+  changeText(){
+    const newText = this.state.text == bold ? Italics: bold;
+    this.setState({ text: newText })
+  }
+  changeColor(){
+    const newColor = this.state.color == red ? blue : red;
+    this.setState({ color: newColor })
+  }
+  render(){
+    return(
+      <div style={{color: this.state.color}}>
+      <p>Change My Color</p>
+      <button onClick={this.changeColor} >Click</button>
+      </div>
+    )
+  }
 }
 
-
-export default App;
+// export default App;
