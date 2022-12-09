b0a29498adc727967f6a806d6655a29343cbad5c
sabahat-123 <sabahat.parveen@imtspl.net>
2022-12-10 02:22:31 +0530

    update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,41 +1,27 @@
 import React, {useEffect, useState} from 'react'
 import '../styles/App.css';
 
-// const App = () => {
-// //code here 
-
-//   return (
-//     <div id="main">
-//       <p className={onchange} >Newton School</p>
-//       <button id='button' onClick={}>Change Style</button>
-//     </div>
-//   )
-// }
-const red = 'red';
-const blue = 'blue';
-
-export default class App extends React.Component{
-  constructor(props){
-    super(props);
-    this.state = { color: red };
-    this.changeColor = this.changeColor.bind(this);
+const App = () => {
+//code here 
+  const [changeStyle , setChangeStyle] = useState("");
+  useEffect(() => {
+    setChangeStyle =("redColor")
+  },[])
+  // setChangeStyle("redColor")
+  const clickHandle = () => {
+    if(changeStyle === "redColor"){
+      setChangeStyle = ("blueColor");
+    }else{
+      setChangeStyle = ("redColor");
+    }
   }
-  changeText(){
-    const newText = this.state.text == bold ? Italics: bold;
-    this.setState({ text: newText })
-  }
-  changeColor(){
-    const newColor = this.state.color == red ? blue : red;
-    this.setState({ color: newColor })
-  }
-  render(){
-    return(
-      <div style={{color: this.state.color}}>
-      <p>Change My Color</p>
-      <button onClick={this.changeColor} >Click</button>
-      </div>
-    )
-  }
+  return (
+    <div id="main">
+      <p className={changeStyle} >Newton School</p>
+      <button id='button' onClick={clickHandle}>Change Style</button>
+    </div>
+  )
 }
 
-// export default App;
+
+export default App;
